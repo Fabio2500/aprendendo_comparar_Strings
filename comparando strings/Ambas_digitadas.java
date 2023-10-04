57b35f0239ba76aafa237d8610045196ab6015fb
Fabio Rafael Dias Viana <102665846+Fabio2500@users.noreply.github.com>
2023-10-04 09:24:37 -0300

    Update Ambas_digitadas.java

--- comparando strings/Ambas_digitadas.java
+++ comparando strings/Ambas_digitadas.java
@@ -4,9 +4,9 @@ class Ambas_digitadas{
 		Scanner sc = new Scanner(System.in);
 		String palavra1,palavra2;
 		System.out.println("digite a primeira palavra:");
-		palavra1 = new String(sc.next().trim());
+		palavra1 = new String(sc.nextLine().trim());
 		System.out.println("agora digite a segunda palavra:");
-		palavra2 = new String(sc.next().trim());
+		palavra2 = new String(sc.nextLine().trim());
 
 		if(palavra1.equals(palavra2)){
 			System.out.println("ambas as palavras são iguais");
